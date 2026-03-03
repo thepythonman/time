bb2a5ebd7b822788b64a7cd7e09785c6483b7129
thepythonman <avim9@proton.me>
2026-03-02 21:26:20 -0500

    added seconds and muinuts and hours debug needed

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 pygame.init()
-clock= pygame.time.Clock()
-b=1
-pastb=0
+clock = pygame.time.Clock()
+minutes = 1
+seconds = 1
+hours = 1
+milliseconds = 0
 while True:
     a = pygame.time.get_ticks()
-    if a == b*60000:
-        b += 1
-    print(b)
+    # milliseconds is below
+    #second is below
+    if a == seconds*1000:
+        seconds += 1
+    # minutes is below 
+    if a >= minutes*60000:
+        minutes += 1
+    # hours is below
+    if a >= hours*3600000:
+        hours += 1
+    print(hours-1, minutes-1, seconds-1)
 pygame.quit()
